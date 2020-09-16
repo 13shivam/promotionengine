4cc67c4ed042744b2f97c14bcd7262fd8bd0e4fe
sr <sr@sr>
2020-09-16 19:17:35 +0530

    Scenario 2 output Formatting

--- src/test/java/com/engine/promotion/promotionengine/impl/CartApplyPromotionImplTest.java
+++ src/test/java/com/engine/promotion/promotionengine/impl/CartApplyPromotionImplTest.java
@@ -88,6 +88,10 @@ public class CartApplyPromotionImplTest {
         MockitoAnnotations.initMocks(this);
         ResponseOrderSKUs responseOrderSKUs = cartApplyPromotion.applyPromotions(cartPromotionsBO);
         Assert.assertEquals("Discount Cart Amount is 370", BigDecimal.valueOf(370l), responseOrderSKUs.getNetAmount());
+        System.out.println("--------------------------------Scenario-2-------------------------------------------------");
+        System.out.println("Before Discount Cart Value:" + cartPromotionsBO.getNetAmount());
+        System.out.println("After Discount Cart Value:" + responseOrderSKUs.getNetAmount());
+        System.out.println("--------------------------------Scenario-2-------------------------------------------------");
     }
 
     //Scenario 3
